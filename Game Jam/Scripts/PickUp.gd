aef407aabcdf79f3234fae79e00f092d5b368295
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-02 19:25:05 -0400

    Air movement

--- Game Jam/Scripts/PickUp.gd
+++ Game Jam/Scripts/PickUp.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 enum ElementState { PARTICLE, WATER, FIRE, AIR, EARTH }
-var type = ElementState.WATER
+var type = ElementState.AIR
 
 func _on_PickUp_body_entered(body):
 	if body.is_in_group("Player"):
