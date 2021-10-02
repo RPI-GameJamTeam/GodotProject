dc446ea3bad9a6ec7a01bdca2bc6c0f69b304c14
dethland <dethlanddeyouxiang@163.com>
2021-10-02 18:54:05 -0400

    add raycast to enemy

--- Game Jam/Scripts/Enemy.gd
+++ Game Jam/Scripts/Enemy.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 enum State { IDLE, PATROL, CHASE }
 var state
 
+var ray_cast_rotating = 18
+
+var player_in_area : bool = false
 var patrolPoints
 
 const FLY_FORCE = 200
@@ -15,10 +18,11 @@ func _ready():
 	state = State.IDLE
 
 func _physics_process(delta):
+
 	var targetPoint = Vector2(1000, -1000)
 	var moveDir = (targetPoint - position).normalized()
 	var movement = moveDir * FLY_FORCE
-	
+
 	# slow the player down while not moving
 	if movement.length() < FLY_FORCE * 0.2:
 		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
@@ -26,7 +30,7 @@ func _physics_process(delta):
 	else:
 		velocity += movement * delta
 		$AnimatedSprite.play("fly")
-	
+
 	if velocity.x < 0:
 		$AnimatedSprite.scale.x = -1
 		rotation = velocity.angle() - deg2rad(180)
@@ -36,9 +40,23 @@ func _physics_process(delta):
 		rotation = velocity.angle()
 	elif velocity.length() == 0:
 		$AnimatedSprite.play("idle")
-	
+
 	# clamp velocity
 	velocity.x = clamp(velocity.x, -FLY_MAX_SPEED, FLY_MAX_SPEED)
 	velocity.y = clamp(velocity.y, -FLY_MAX_SPEED, FLY_MAX_SPEED)
 
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _on_CheckPlayer_body_entered(body):
+	player_in_area = true
+
+
+func _on_CheckPlayer_body_exited(body):
+	player_in_area = false
+	
+func _process(delta):
+	if player_in_area:
+		if $RayCast2D.is_colliding() or $RayCast2D2.is_colliding():
+			print('find it')
+
