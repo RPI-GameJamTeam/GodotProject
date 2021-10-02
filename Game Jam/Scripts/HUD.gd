c839492be6a7e92d65e55ed834cf9f17e40d33a8
dethland <dethlanddeyouxiang@163.com>
2021-10-02 16:30:20 -0400

    avatar update

--- Game Jam/Scripts/HUD.gd
+++ Game Jam/Scripts/HUD.gd
@@ -2,6 +2,10 @@ extends Control
 
 # this is the hud control node
 export var health_reduction : int
+const AVATAR_LOCATION = {'air':"res://Asset/Avatar/air_ele.png", "ground":"res://Asset/Avatar/earth_ele.png",
+						'fire':"res://Asset/Avatar/fire_ele.png", "water":"res://Asset/Avatar/water_ele.png"}
+
+
 
 enum ElementState { PARTICLE, WATER, FIRE, AIR, GROUND }
 signal health_run_out()
@@ -9,12 +13,11 @@ signal health_run_out()
 func _ready():
 	self.show()
 	get_parent().get_parent().get_node("Player").connect("ElementTransition",self, "onElementChanged")
-
 func onElementChanged(value):
-	
+
 	match value:
 		ElementState.PARTICLE:
-			pass
+			$frame/avatar.set_texture(load(AVATAR_LOCATION['fire']))
 		ElementState.AIR:
 			pass
 		ElementState.FIRE:
