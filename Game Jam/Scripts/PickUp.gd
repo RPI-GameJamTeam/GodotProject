3b17f27339c971dcd78514849c620f79ca359d79
dethland <dethlanddeyouxiang@163.com>
2021-10-02 15:38:34 -0400

    add more animation for character
    fall and jump animation

--- Game Jam/Scripts/PickUp.gd
+++ Game Jam/Scripts/PickUp.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 enum ElementState { PARTICLE, WATER, FIRE, AIR, GROUND }
-var type = ElementState.EARTH
+var type = ElementState.GROUND
 
 func _on_PickUp_body_entered(body):
 	if body.is_in_group("Player"):
