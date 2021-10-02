230bc89341b413793f9cb575d659bd1be0d3f243
dethland <dethlanddeyouxiang@163.com>
2021-10-02 14:40:13 -0400

    Hud add to the main

--- Game Jam/Scripts/HUD.gd
+++ Game Jam/Scripts/HUD.gd
@@ -1,16 +1,23 @@
 extends Control
 
+# this is the hud control node
+export var health_reduction : int
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+enum ElementState { PARTICLE, WATER, FIRE, AIR, GROUND }
+signal health_run_out()
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.show()
+	get_parent().get_parent().get_node("Player").connect("ElementTransition",self, "onElementChanged")
 
+func onElementChanged(value):
+	match value:
+		ElementState.PARTICLE:
+			pass
+		ElementState.AIR:
+			pass
+		ElementState.FIRE:
+			pass
+		ElementState.GROUND:
+			pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
