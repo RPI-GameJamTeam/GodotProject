dce948975a4743a0ae27a8e2690f90b7ca0793b7
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-03 04:53:25 -0400

    TitleScreen tweaks

--- Game Jam/Scripts/TitleSreen.gd
+++ Game Jam/Scripts/TitleSreen.gd
@@ -1,11 +1,11 @@
 extends Control
 
-
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		$AnimationPlayer.play("fade away")
 
 func _on_Button_pressed():
 	$AnimationPlayer.play("fade away")
-	
-
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene("res://Scenes/MainScene.tscn")
