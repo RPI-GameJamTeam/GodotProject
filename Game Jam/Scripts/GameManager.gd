d12c94c4607985eed43777e85a4be4b9df41ade2
dethland <dethlanddeyouxiang@163.com>
2021-10-03 02:08:32 -0400

    add the main scene load function

--- Game Jam/Scripts/GameManager.gd
+++ Game Jam/Scripts/GameManager.gd
@@ -1,8 +1,18 @@
 extends Node
 
+export var Level_index : int = 1
+
 func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	elif Input.is_action_pressed("restart"):
 		get_tree().get_nodes_in_group("Player")[0].die()
+		
+		
+func level_changer(level_index):
+	
+	var Level = load("res://Level/level"+str(level_index)+".tscn").instance()
+	self.add_child(Level)
 
+func _ready():
+	level_changer(Level_index)
