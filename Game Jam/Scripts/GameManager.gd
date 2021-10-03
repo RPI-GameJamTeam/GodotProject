902891f0429015a7a518074fbca4946775ffb277
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-02 22:15:19 -0400

    Prevent dashing into walls

--- Game Jam/Scripts/GameManager.gd
+++ Game Jam/Scripts/GameManager.gd
@@ -4,7 +4,5 @@ func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	elif Input.is_action_pressed("restart"):
-		reset()
+		get_tree().get_nodes_in_group("Player")[0].die()
 
-func reset():
-	pass
