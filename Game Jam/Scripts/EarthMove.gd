79e121bfe2b20b4c59b3402cc7d242875d41640a
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-06 23:52:52 -0400

    Fix earth movement

--- Game Jam/Scripts/EarthMove.gd
+++ Game Jam/Scripts/EarthMove.gd
@@ -9,10 +9,22 @@ var velocity : Vector2
 var localGrounded
 var localWasGrounded
 
-func _ready():
-	player = get_parent().get_parent()
+var tilemap
+var tileSize
+var tilePos
+var tileIndex
 
-func _physics_process(delta):	
+func _ready():
+	player = get_parent().get_parent()	
+	tilemap = get_tree().get_nodes_in_group("Tilemap")[0]
+	var tile_rect = tilemap.get_used_rect()
+	tileSize = tilemap.map_to_world(tile_rect.size)
+
+func _physics_process(delta):
+#	var test = load("res://Scenes/DebugCircle.tscn").instance()
+#	player.get_parent().add_child(test)
+#	test.global_position = player.global_position
+	
 	if !is_active:
 		return null
 	
@@ -27,6 +39,8 @@ func _physics_process(delta):
 	var movement = Vector2((player.input * player.WALK_FORCE).x, 0)
 	localWasGrounded = localGrounded
 	localGrounded = player.get_node("DownCast").is_colliding()
+	var leftGrounded = player.get_node("DownLeftCast").is_colliding()
+	var rightGrounded = player.get_node("DownRightCast").is_colliding()
 	
 	# slow the player down while not moving
 	var cond1 = abs(movement.x) < player.WALK_FORCE * 0.2
@@ -42,10 +56,10 @@ func _physics_process(delta):
 		player.get_node("AnimatedSprite").scale.x = -1
 	elif movement.x > 0:
 		player.get_node("AnimatedSprite").scale.x = 1
-	elif movement.x == 0 and localGrounded:
+	elif movement.x == 0 and localGrounded or rightGrounded or leftGrounded:
 		player.get_node("AnimatedSprite").play("idle")
 	
-	if !localGrounded:
+	if !localGrounded and !rightGrounded and !leftGrounded:
 		if velocity.y < 0:
 			player.get_node("AnimatedSprite").play("jump")
 		else:
@@ -54,28 +68,57 @@ func _physics_process(delta):
 	# clamp velocity
 	velocity.x = clamp(velocity.x, -player.WALK_MAX_SPEED, player.WALK_MAX_SPEED)
 
-	# apply gravity
-	if (!localGrounded) and !attemptedRotation:
-		if !localWasGrounded:
-			pass
-			#player.rotation = 0
-			#velocity.y += player.gravity * 4 * delta * 2
-		else:
-			var leftGrounded = player.get_node("DownLeftCast").is_colliding()
-			var rightGrounded = player.get_node("DownRightCast").is_colliding()
-			
-			if leftGrounded and !rightGrounded: # rotate clockwise
-				player.rotation += deg2rad(90)
-			elif !leftGrounded and rightGrounded: # rotate counterclockwise
-				player.rotation += deg2rad(-90)
-			else:
-				print("fall")
-
 	# move
 	velocity = player.move_and_slide_with_snap(velocity.rotated(player.rotation), Vector2.DOWN, Vector2.UP).rotated(-player.rotation)
 	velocity.x = round(velocity.x)
 	velocity.y = round(velocity.y)
+	
+	var newTilePos = tilemap.world_to_map(player.position + (player.get_node("DownCast").cast_to + Vector2(0, 10)).rotated(player.rotation))
+	var newTileIndex = tilemap.get_cellv(newTilePos)
+	
+	var nearest90 = int(rad2deg(round(player.rotation / deg2rad(45)) * deg2rad(45)))
+	if newTileIndex == -1 and tileIndex != -1 and nearest90 != -180 and nearest90 != 180:
+		# get the center of the tile in world coords
+		var tileWorldPos = tilemap.map_to_world(tilePos) + Vector2(16, 16) # 32x32 tiles, so 16 give us center
+		var distance_from_point = player.global_position - tileWorldPos
+		
+		var phi = atan2(distance_from_point.y, distance_from_point.x)
+		var nearest45 = round(phi / deg2rad(45)) * deg2rad(45);
+		phi = phi - nearest45
+		var theta = phi
+		var rotationAngle = -2*theta
+		
+		var rotated = false
+		
+		if leftGrounded and !rightGrounded: # rotate clockwise
+			var xNew = distance_from_point.x * cos(rotationAngle) - distance_from_point.y * sin(rotationAngle)
+			var yNew = distance_from_point.x * sin(rotationAngle) + distance_from_point.y * cos(rotationAngle)
+			
+			player.global_position = Vector2(tileWorldPos + Vector2(xNew, yNew))
+			player.rotation += deg2rad(90)
+			
+			rotated = true
+		elif !leftGrounded and rightGrounded: # rotate counterclockwise			
+			var xNew = distance_from_point.x * cos(rotationAngle) - distance_from_point.y * sin(rotationAngle)
+			var yNew = distance_from_point.x * sin(rotationAngle) + distance_from_point.y * cos(rotationAngle)
+			
+			player.global_position = Vector2(tileWorldPos + Vector2(xNew, yNew))
+			player.rotation += deg2rad(-90)
+			
+			rotated = true
+		
+		if rotated:
+			velocity.x = 0
+			velocity.y = 0
+	else:
+		if !localGrounded and !attemptedRotation and !leftGrounded and !rightGrounded:
+			player.rotation = 0
+			velocity.y += player.gravity * 4 * delta * 2
+	
+	tilePos = newTilePos
+	tileIndex = newTileIndex
+
 
 	if localGrounded and Input.is_action_just_pressed("jump"):
 		velocity.y += -player.JUMP_SPEED
-	
+
