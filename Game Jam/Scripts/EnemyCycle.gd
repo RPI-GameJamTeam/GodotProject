fd80600633d6643757956ab1f834726695b1b661
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-08 14:45:26 -0400

    Flip Enemy sprite appropriately based on its movement direction

--- Game Jam/Scripts/EnemyCycle.gd
+++ Game Jam/Scripts/EnemyCycle.gd
@@ -3,5 +3,21 @@ extends PathFollow2D
 
 export var moveing_speed = 100
 
+var prevPos
+
+func _ready():
+	prevPos = $Enemy.global_position
+
 func _process(delta):
+	var currentPos = $Enemy.global_position
 	set_offset(get_offset() + moveing_speed * delta)
+	
+	var dir = currentPos.x - prevPos.x
+	
+	if dir < 0:
+		$Enemy/AnimatedSprite.set_flip_v(true)
+	else:
+		$Enemy/AnimatedSprite.set_flip_v(false)
+	
+	print(currentPos.x - prevPos.x)
+	prevPos = currentPos
