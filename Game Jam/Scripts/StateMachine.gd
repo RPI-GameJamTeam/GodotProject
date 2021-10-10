360ac7ab65faa0deb39db31f848ad03e1f250954
dethland <dethlanddeyouxiang@163.com>
2021-10-09 20:31:18 -0400

    add clippling fixer change some tile texture

--- Game Jam/Scripts/StateMachine.gd
+++ Game Jam/Scripts/StateMachine.gd
@@ -4,6 +4,7 @@ var on_floor : bool = false
 var right_contact : bool = false
 var left_contact : bool = false
 var top_contact : bool = false
+var clipping : bool = false
 
 var grounded
 var groundedLeft
@@ -26,6 +27,14 @@ func _physics_process(_delta):
 	grounded = get_parent().get_node("DownCast").is_colliding()
 	groundedLeft = get_parent().get_node("DownLeftCast").is_colliding()
 	groundedRight = get_parent().get_node("DownRightCast").is_colliding()
+	
+	clipping_fixer()
+	
+func clipping_fixer():
+	if clipping:
+		# this fucntion only fix clipping to right head wall
+		# to compelte the fucntion need the head_up direction for the player
+		get_parent().position.x += 0.5
 
 
 func _on_Top_body_entered(_body):
@@ -51,3 +60,11 @@ func _on_Right_body_exited(_body):
 
 func _on_Left_body_exited(_body):
 	left_contact = false
+
+func _on_Inner_body_entered(_body):
+	clipping = true
+	print('Not good Im inside the wall!')
+
+func _on_Inner_body_exited(_body):
+	clipping = false
+	print('Im outside the wall, yeah!')
