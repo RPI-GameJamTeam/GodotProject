29b8a61f78e6127ef4012c45c61fd8f5bd1f8be4
dethland <dethlanddeyouxiang@163.com>
2021-10-19 13:40:10 -0400

    Elevator moving update

--- Game Jam/Scripts/InteractItem/Elevator.gd
+++ Game Jam/Scripts/InteractItem/Elevator.gd
@@ -1,6 +1,52 @@
-extends KinematicBody2D
+extends Node2D
 
 export(int) var speed = 1
+export(float) var distanceMargin = 0.5
+export(PoolVector2Array) var targetPoints
+export(Curve) var movingBuffer
+
+var targetPointIndex = 0
+var originalPosition : Vector2
+
+onready var elevator = $KinematicBody2D
+
+func _ready():
+	targetPoints.insert(0, Vector2.ZERO)
+	originalPosition = elevator.position
 
 func _physics_process(delta):
-	position.y -= delta * speed
+#	for point in targetPoints:
+#		var currentePosition = self.position
+#		var moveTo : Vector2 = point - currentePosition
+#		var moveToDir = moveTo.normalized()
+#		if currentePosition.direction_to(point):
+
+#	position.y -= delta * speed
+	move_to(targetPoints[targetPointIndex], delta)
+	if check_on_target(targetPoints[targetPointIndex]):
+		
+		if targetPoints.size() > targetPointIndex + 1:
+			originalPosition = targetPoints[targetPointIndex]
+			targetPointIndex += 1
+		else:
+			originalPosition = targetPoints[targetPointIndex]
+			targetPointIndex = 0
+
+
+
+func move_to(target_position, delta):
+	var moveToDir : Vector2 = (target_position - elevator.position).normalized()
+	var moveProcess = abs((elevator.position - originalPosition).length()/(target_position - originalPosition).length()) if (target_position - originalPosition).length() !=0 else 1
+#	print(moveProcess)
+	var buffer : float = movingBuffer.interpolate(moveProcess)
+	elevator.position += moveToDir * speed * delta * buffer
+
+
+func check_on_target(target_position):
+	var result : bool
+	var distanceToTarget = (target_position - elevator.position).length()
+	if distanceToTarget < distanceMargin:
+		result = true
+	else:
+		result = false
+	return result
