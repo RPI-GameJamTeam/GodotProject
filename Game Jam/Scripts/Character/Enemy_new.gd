49af7783d225c1847e22b68746a41846e460cf77
dethland <dethlanddeyouxiang@163.com>
2021-10-29 17:44:36 -0400

    Enemy control update
    clean enemy node only need the enemy_new to work

--- Game Jam/Scripts/Character/Enemy_new.gd
+++ Game Jam/Scripts/Character/Enemy_new.gd
@@ -1,25 +1,34 @@
-tool
+
 extends Node2D
 
 export(int) var speed = 1
-export(float) var distanceMargin = 0.5
+export(float) var angularSpeed = deg2rad(5)
 export(PoolVector2Array) var targetPoints
 export(Curve) var movingBuffer
 export(Vector2) var scale_for_sprite
 
+var distanceMargin = 0.5
+var angularMargin = deg2rad(5)
+
 var targetPointIndex = 0
 var originalPosition : Vector2
+var faceDir : Vector2
 
-onready var elevator = $Enemy
+onready var enemy = $Enemy
 
 func _ready():
+	faceDir = Vector2.RIGHT.rotated(rotation)
 	if targetPoints[0] != Vector2.ZERO:
 		targetPoints.insert(0, Vector2.ZERO)
-	originalPosition = elevator.position
-	elevator.scale = scale_for_sprite
+	originalPosition = enemy.position
+	enemy.scale = scale_for_sprite
+	print(Vector2.LEFT.angle_to(Vector2.DOWN))
 
 func _physics_process(delta):
+	faceDir = Vector2.RIGHT.rotated(rotation).rotated(enemy.rotation)
+	print(faceDir)
 	move_to(targetPoints[targetPointIndex], delta)
+	turn_to(targetPoints[targetPointIndex], delta)
 	if check_on_target(targetPoints[targetPointIndex]):
 		if targetPoints.size() > targetPointIndex + 1:
 			originalPosition = targetPoints[targetPointIndex]
@@ -29,16 +38,32 @@ func _physics_process(delta):
 			targetPointIndex = 0
 
 func move_to(target_position, delta):
-	var moveToDir : Vector2 = (target_position - elevator.position).normalized()
-	var moveProcess = abs((elevator.position - originalPosition).length()/(target_position - originalPosition).length()) if (target_position - originalPosition).length() !=0 else 1
-#	print(moveProcess)
+	var moveToDir : Vector2 = (target_position - enemy.position).normalized()
+	var moveProcess = abs((enemy.position - originalPosition).length()/(target_position - originalPosition).length()) if (target_position - originalPosition).length() !=0 else 1
 	var buffer : float = movingBuffer.interpolate(moveProcess)
-	elevator.position += moveToDir * speed * delta * buffer
+	enemy.position += moveToDir * speed * delta * buffer
 
+func turn_to(target_position, delta):
+	# get the angel between enemy toward the target position
+	var moveToRadius = faceDir.angle_to(target_position - enemy.position)
+#	print(enemy.position)
+	if moveToRadius > angularMargin:
+		enemy.rotation += angularSpeed * delta
+	elif moveToRadius < -angularMargin:
+		enemy.rotation -= angularSpeed * delta
+	else:
+		faceDir = (target_position - enemy.position).normalized()
+	# turn if the rotation of the enemy is smaller than the target position
+#	if moveToRadius - enemy.rotation > angularMargin:
+#		enemy.rotation_degrees += angularSpeed * delta
+		# reset to zero when rotate fully 
+#		if enemy.rotation_degrees - 360 < 0.5:
+#			enemy.rotation_degrees = 0
+	
 
 func check_on_target(target_position):
 	var result : bool
-	var distanceToTarget = (target_position - elevator.position).length()
+	var distanceToTarget = (target_position - enemy.position).length()
 	if distanceToTarget < distanceMargin:
 		result = true
 	else:
