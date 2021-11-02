1baf2663deaa3cf002d84cf0766ba23554841a60
dethland <dethlanddeyouxiang@163.com>
2021-11-02 16:46:27 -0400

    editor zoom function added

--- Game Jam/Scripts/Editor/LevelEditor.gd
+++ Game Jam/Scripts/Editor/LevelEditor.gd
@@ -7,7 +7,7 @@ extends Node2D
 # delete tool
 # select tool
 
-enum viewportMode {FOLLOW, ZOOM_UP, ZOOM_DOWN, IDLE}
+enum viewportMode {FOLLOW, IDLE}
 var curViewMode = viewportMode.IDLE
 var mouseOrgPosition : Vector2
 var cameraOrgPosition : Vector2
@@ -15,45 +15,46 @@ onready var camera = $Camera2D
 
 func _ready():
 	var groups = ['TileGroup', 'PlayerGroup', 'EnemyGroup', 'DecorationGroup', 'ObstacalGroup']
-	 
+
 	for group in groups:
 		var newGroup = Node2D.new()
 		newGroup.name = group
 		$Level.add_child(newGroup)
 		newGroup.owner = $Level
-		
-	
+
+
 func _input(event):
-	
+
 	# screen control
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == BUTTON_WHEEL_DOWN:
-				curViewMode = viewportMode.ZOOM_DOWN
+				camera.zoom += Vector2(0.1, 0.1)
+				
+
 			if event.button_index == BUTTON_WHEEL_UP:
-				curViewMode = viewportMode.ZOOM_UP
+				camera.zoom -= Vector2(0.1, 0.1)
+				
+				
 			if event.button_index == BUTTON_MIDDLE:
 				mouseOrgPosition = get_viewport().get_mouse_position()
 				cameraOrgPosition = camera.position
 				curViewMode = viewportMode.FOLLOW
 		else:
 			curViewMode = viewportMode.IDLE
-			
-	if event is InputEventMouseMotion:
-#		print(event.position)	
-		pass
-		
-func _process(delta):
+
+	camera.zoom.x = clamp(camera.zoom.x, 0.5, 3)
+	camera.zoom.y = clamp(camera.zoom.y, 0.5, 3)
+	print(camera.zoom)
 	
+
+func _process(delta):
+
 	match curViewMode:
 		viewportMode.FOLLOW:
 			var mouseCurPosition = get_viewport().get_mouse_position()
-			var shiftVec = mouseCurPosition - mouseOrgPosition
+			var shiftVec = (mouseCurPosition - mouseOrgPosition) * camera.zoom.y
 			camera.position = cameraOrgPosition - shiftVec
-		viewportMode.ZOOM_DOWN:
-			pass
-		viewportMode.ZOOM_UP:
-			pass
 		viewportMode.IDLE:
 			pass
-	
+
